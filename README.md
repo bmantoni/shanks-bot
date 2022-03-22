8a407001c8ef43bca0ef075a8e3685a6ade99642
Bobby Mantoni <bobby@mantoni.me>
2022-03-22 16:25:35 +0000

    working

---
+++ README.md
@@ -0,0 +1,8 @@
+# Repeating Reciprocals
+
+Inspired by [this Numberphile video|https://www.youtube.com/watch?v=DmfxIhmGPP4] and [William Shanks|https://en.wikipedia.org/wiki/William_Shanks].
+
+Given an input number, calculates the number of digits of the reciprocal until it repeats.
+
+Example: `node shanks-bot.js 60029`
+
---
+++ shanks-bot.js
@@ -0,0 +1,50 @@
+let input = parseInt(process.argv[2])
+
+class RepeatingReciprocalFinder {
+
+    constructor(n) {
+        this.n = n;
+        this.pastRemainders = []
+        this.quotientDigits = 0
+    }
+
+    findDividend() {
+        var dividend = 1
+        while (this.n > dividend) dividend *= 10
+        return dividend
+    }
+
+    findGreatestMultiple(butNotGreaterThan) {
+        var sum = 0
+        var multiples = 0
+        while ((sum + input) < butNotGreaterThan) {
+            sum += input
+            ++multiples
+        }
+        return sum // multiples would go into the quotient but we don't need it
+    }
+
+    getDigitsUntilReciprocalRepeats() {
+        var currentDividend = this.findDividend();
+        var repeating = false;
+
+        while (!repeating) {
+            var multiple = this.findGreatestMultiple(currentDividend)
+            var remainder = currentDividend - multiple
+            if (!this.pastRemainders.includes(remainder)) {
+                this.pastRemainders.push(remainder)
+                //console.log(this.pastRemainders)
+                ++this.quotientDigits
+                currentDividend = remainder * 10;
+            } else {
+                repeating = true
+            }
+        }
+        return this.quotientDigits
+    }
+}
+
+let finder = new RepeatingReciprocalFinder(input);
+let result = finder.getDigitsUntilReciprocalRepeats()
+
+console.log(`The reciprocal of ${input} repeats after ${result} digits`);
